16fe064a85163ba23e8ae03a9b1585c970f91360
arrowira <41136800+arrowira@users.noreply.github.com>
2025-07-26 19:36:21 -0700

    music and hit detection with ship

---
+++ spacejam/Scenes/playerCollision.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if area.name == "radarItemS":
+		#damaged
+		area.get_parent().queue_free()
+		print("you got hit!")
--- spacejam/Scenes/spawner.gd
+++ spacejam/Scenes/spawner.gd
@@ -15,7 +15,7 @@ var debrisSpawnAngle = 40
 func _on_spawn_cool_down_timeout() -> void:
 	$spawnCoolDown.start()
 	var angle = deg_to_rad((randf_range(-1,1)*debrisSpawnAngle)-180)
-	print(angle)
+	
 	var newDebris = debris.instantiate()
 	newDebris.position = Vector2(sin(angle)*spawnLength,cos(angle)*spawnLength)
 	get_parent().get_node("background").add_child(newDebris)
